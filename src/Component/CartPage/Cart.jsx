589719e2b7d5a5bdbb4556b5cbb44be7dc7bfc33
naybala <naybalaismylife.nbl@gmail.com>
2022-07-22 23:17:33 +0630

    cart page ui

--- src/Component/CartPage/Cart.jsx
+++ src/Component/CartPage/Cart.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import './Cart.css'
-export default function Cart() {
+export default function Cart(props) {
     return (
         <>
-            <h1>This is Cart Page</h1>
+            <div className="container">
+                <table class="table table-dark table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Product Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Amount</th>
+                            <th scope="col">Sub Amount</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            props.cart.map((d) => (
+                                <tr key={d.id}>
+                                    <th>{d.title}</th>
+                                    <td>{d.price} $</td>
+                                    <td>{d.qty}</td>
+                                    <td>{d.qty * d.price} $</td>
+                                    <td >
+                                        <button className='btn btn-sm btn-primary me-2 d-inline-block'><i class="bi bi-dash-circle-fill"></i></button>
+                                        <button className='btn btn-sm btn-primary'><i class="bi bi-plus-circle-fill"></i></button>
+                                    </td>
+                                    <td><button className='btn btn-danger'>remove <i class="bi bi-trash3-fill"></i></button></td>
+                                </tr>
+                            ))
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
