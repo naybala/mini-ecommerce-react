538938a139827173b98150204d5773a54ed01d23
naybala <naybalaismylife.nbl@gmail.com>
2022-07-23 13:51:46 +0630

    commit

--- src/Component/CartPage/Cart.jsx
+++ src/Component/CartPage/Cart.jsx
@@ -1,16 +1,48 @@
 import React from 'react'
 import './Cart.css'
 export default function Cart(props) {
+
+    const minus = (product) => {
+        props.setCart((prevState) => prevState.map((d) => {
+            if (d.id === product.id) {
+                if (d.qty !== 1) {
+                    var updateQty = d.qty - 1;
+                    return { ...d, qty: updateQty };
+                } else {
+                    d.qty = 1;
+                }
+            }
+            return d;
+        })
+        )
+    }
+
+    const plus = (product) => {
+        props.setCart((prevState) => prevState.map((d) => {
+            if (d.id === product.id) {
+                var updateQty = d.qty + 1;
+                return { ...d, qty: updateQty };
+            }
+            return d;
+        })
+        );
+    }
+
+    const remove = (product) => {
+        console.log("hello");
+    }
+
+
     return (
         <>
             <div className="container">
-                <table class="table table-dark table-striped">
+                <table className="table table-dark table-striped table-hover border-2">
                     <thead>
                         <tr>
-                            <th scope="col">Product Name</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Amount</th>
-                            <th scope="col">Sub Amount</th>
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>Amount</th>
+                            <th>Sub Amount</th>
                             <th></th>
                             <th></th>
                         </tr>
@@ -24,10 +56,10 @@ export default function Cart(props) {
                                     <td>{d.qty}</td>
                                     <td>{d.qty * d.price} $</td>
                                     <td >
-                                        <button className='btn btn-sm btn-primary me-2 d-inline-block'><i class="bi bi-dash-circle-fill"></i></button>
-                                        <button className='btn btn-sm btn-primary'><i class="bi bi-plus-circle-fill"></i></button>
+                                        <button onClick={() => minus(d)} className='btn btn-sm btn-primary me-2 d-inline-block'><i className="bi bi-dash-circle-fill"></i></button>
+                                        <button onClick={() => plus(d)} className='btn btn-sm btn-primary'><i className="bi bi-plus-circle-fill"></i></button>
                                     </td>
-                                    <td><button className='btn btn-danger'>remove <i class="bi bi-trash3-fill"></i></button></td>
+                                    <td><button onClick={() => remove(d)} className='btn btn-danger'>remove <i className="bi bi-trash3-fill"></i></button></td>
                                 </tr>
                             ))
                         }
